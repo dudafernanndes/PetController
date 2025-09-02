c13f3a72e728ef3fdfa9572d02ac9cd92030c2a7
dudafernanndes <dev.dudafernandes@gmail.com>
2025-09-02 14:58:13 -0300

    Transferindo arquivo do Spring Initializr para o GitHub

---
+++ pet-api/src/main/java/br/com/fiap/pet_api/PetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetApiApplication.class, args);
+	}
+
+}
